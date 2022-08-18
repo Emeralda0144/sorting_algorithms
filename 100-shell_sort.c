8b760a04b3764ad78b5182fbf82501c0cbb34d01
Emeralda0144 <kwunder014@gmail.com>
2022-08-18 03:47:13 -0700

    100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,38 +1,47 @@
 #include "sort.h"
 
-void geusa_ints(int *a, int *b)
+/**
+ * get_max_gap - gets the a largest Knuth Sequence gap for this size
+ * @size: the size of the array
+ *
+ * Return: the gap size
+ */
+size_t get_max_gap(size_t size)
 {
-	int tmp;
+	size_t n;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	n = 1;
+	while (n < size)
+		n = n * 3 + 1;
+	return ((n - 1) / 3);
 }
 
 /**
- * shell sort
- * Description: sorts an array of integers
+ * shell_sort - shell_sort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t dif, i, j;
+	size_t gap, i, j;
+	int temp;
 
-	if (array == NULL || size < 2)
+	if (!array || !size)
 		return;
 
-	for (dif = 1; dif < (size / 3);)
-		dif = dif * 3 + 1;
 
-	for (; dif >= 1; dif /= 3)
+	for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
 	{
-		for (i = dif; i < size; i++)
+		for (i = gap; i < size; i++)
 		{
-			j = i;
-			while (j >= dif && array[j - dif] > array[j])
+			temp = array[i];
+			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
 			{
-				geusa_ints(array + j, array + (j - dif));
-				j -= dif;
+				array[j] = array[j - gap];
 			}
+			array[j] = temp;
 		}
 		print_array(array, size);
 	}
