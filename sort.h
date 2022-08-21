6137beff4bc41b0e73402e1e2cd0070187f3d134
Emeralda0144 <kwunder014@gmail.com>
2022-08-20 23:10:38 -0700

    sort.h

--- sort.h
+++ sort.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Directional macros for compare sort */
+/* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
 
@@ -33,15 +33,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* These are printing helper functions for the tests*/
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sorting algoritms protostypes */
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H */
